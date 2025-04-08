80f64db1921873f0cae77835abe780f38d98a479
COrollaROOT <doctor6749@gmail.com>
2025-04-08 12:11:01 +0900

    [feat] 타이틀 씬 추가
    베이스 씬 을 상속 받는 타이틀 씬 구현

---
+++ OOPConsoleProject/OOPConsoleProject/Scenes/TitleScene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject.Scenes
+{
+    public class TitleScene : BaseScene // 추상클래스를 상속받는 클래스 구현
+    {
+        public override void Render()
+        {
+            
+        }
+
+        public override void Input()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Result()
+        {
+            
+        }
+    }
+}
