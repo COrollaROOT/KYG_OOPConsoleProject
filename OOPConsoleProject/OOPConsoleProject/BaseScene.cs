1873a9d7f3991582b1b184db575615362ecf9d18
COrollaROOT <doctor6749@gmail.com>
2025-04-08 11:55:06 +0900

    [feat] 베이스씬 구현
    베이스가 될 씬 구현하고 앞으로 나올 여러씬들이 상속받을수있게 코드 수정

--- OOPConsoleProject/OOPConsoleProject/BaseScene.cs
+++ OOPConsoleProject/OOPConsoleProject/BaseScene.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace OOPConsoleProject
 {
-    public static class BaseScene
+    public abstract class BaseScene // 씬을 상속 받아 여러 씬 구현할 예정 때문에 추상 클래스로 구현
     {
+
+        public abstract void Render();
+
+        public abstract void Input();
+
+        public abstract void Update();
+
+        public abstract void Result();
+
+
+
     }
 }
--- OOPConsoleProject/OOPConsoleProject/Game.cs
+++ OOPConsoleProject/OOPConsoleProject/Game.cs
@@ -8,11 +8,20 @@ namespace OOPConsoleProject
 {
     public static class Game // 게임 시
     {
+        private static Dictionary<string, BaseScene> sceneDic; // 베이스 씬 여러게 있는 씬 사전
+        private static BaseScene curScene; // 씬 변경되기 때문에 현재씬 구동
 
+        private static bool gameOver; // 게임 오버 (참/거짓)
 
         public static void Run() // 구동
         {
-
+            while (gameOver == false) // 게임 오버가 아니면 계속 반복
+            {
+                curScene.Render();
+                curScene.Input();
+                curScene.Update();
+                curScene.Result();
+            }
         }
     }
 }
